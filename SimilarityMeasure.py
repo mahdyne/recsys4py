3e2d25e696ba5b951471631676e69df13947a5c9
mahdi.nematpour <mahdi.nematpour@pintapin.com>
2019-08-09 21:43:38 +0430

    fix reset_index bug

--- SimilarityMeasure.py
+++ SimilarityMeasure.py
@@ -4,8 +4,7 @@ class JaccardSim:
         self.df_preferences = df_preferences
 
     def calcSim(self):
-        df_user_pref_count=self.df_preferences.groupby('user_id').count().rename(columns={'item_id':'pref_count'})
-        print(df_user_pref_count)
+        df_user_pref_count=self.df_preferences.groupby('user_id').count().reset_index().rename(columns={'item_id':'pref_count'})
         df_user_intersection=pd.merge(self.df_preferences,self.df_preferences,left_on=['item_id'],right_on=['item_id'])
         df_users_intersection_asym=df_user_intersection[df_user_intersection.user_id_x<df_user_intersection.user_id_y]
         df_users_intersection_count=df_users_intersection_asym.groupby(['user_id_x','user_id_y']).count().reset_index().rename(columns={'item_id':'intersection_count'})
